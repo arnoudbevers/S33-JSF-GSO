5ca7e1a890560a0421b98ac9a0e585114a77817c
bramhoven <bram.hoven@hotmail.com>
2017-03-29 13:48:16 +0200

    Addid

--- UnittestenTime/src/fontys/time/Appointment.java
+++ UnittestenTime/src/fontys/time/Appointment.java
@@ -5,10 +5,58 @@
  */
 package fontys.time;
 
+import java.util.List;
+
 /**
  *
- * @author arnoudbevers
+ * @author arnienoob
  */
 public class Appointment {
     
+	private String subject;
+	private ITimeSpan timespan;
+	private List<Contact> invitees;
+
+	/**
+	 * Create an appointment with a subject and a timespan
+	 * @param subject
+	 * @param timespan
+	 */
+	public Appointment(String subject, ITimeSpan timespan) {
+		this.subject = subject;
+		this.timespan = timespan;
+	}
+	
+	public String getSubject() {
+		return this.subject;
+	}
+	
+	public ITimeSpan getTimeSpan() {
+		return this.timespan;
+	}
+	
+	public List<Contact> getInvitees() {
+		return this.invitees;
+	}
+
+	/**
+	 * Adds a contact to the Appointment but only if the contact has no intervering appointments
+	 * @param Contact 
+	 * @return boolean if the addition was completed succesfully
+	 */
+	public boolean addContact(Contact c) {
+		boolean noConflict = true;
+		for(Appointment a : c.appointments) {
+			noConflict = a.getTimeSpan().intersectionWith(getTimeSpan()) != null;
+			if(!noConflict) break;
+		}
+		if(noConflict) {
+			this.invitees.add(c);
+		}
+		return noConflict;
+	}
+	
+	public void removeContact(Contact c) {
+		this.invitees.remove(c);
+	}
 }
