2e8804e6f95e3022549eef0ff753c07ce17efc9e
Arnoud Bevers <346957@student.fontys.nl>
2017-04-06 09:39:08 +0200

    [ADDED] Implementation of AEXBanner Class Diagram

---
+++ AEXBanner/src/aexbanner/BannerController.java
@@ -0,0 +1,12 @@
+package aexbanner;
+
+/**
+ *
+ * @author arnoudbevers
+ */
+public class BannerController {
+    public BannerController(AEXBanner banner){
+        
+    }
+    
+}
---
+++ AEXBanner/src/aexbanner/MockEffectenBeurs.java
@@ -0,0 +1,17 @@
+package aexbanner;
+
+import java.util.List;
+
+/**
+ *
+ * @author arnoudbevers
+ */
+public class MockEffectenBeurs implements IEffectenBeurs{
+
+    @Override
+    public List<IFonds> getKoersen() {
+        //TODO - implement method
+        return null;
+    }
+    
+}
