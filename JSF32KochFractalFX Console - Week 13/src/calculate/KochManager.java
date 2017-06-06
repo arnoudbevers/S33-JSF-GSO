ae55a790a6b8f7a6f63ec502c5eb3d22a7990b79
Arnoud Bevers <346957@student.fontys.nl>
2017-06-06 15:10:17 +0200

    Beter werkt het nu a mattie

--- JSF32KochFractalFX Console - Week 13/src/calculate/KochManager.java
+++ JSF32KochFractalFX Console - Week 13/src/calculate/KochManager.java
@@ -24,6 +24,7 @@ import javafx.scene.paint.Color;
 import timeutil.TimeStamp;
 
 public class KochManager {
+
     private volatile List<Edge> edges;
     public int count = 0;
     public ExecutorService pool;
@@ -31,19 +32,18 @@ public class KochManager {
     private RandomAccessFile raf;
     private FileChannel fc;
     private TimeStamp ts = new TimeStamp();
-	
 
     public KochManager(String outputFile) {
         this.edges = new ArrayList<Edge>();
         this.pool = Executors.newFixedThreadPool(4);
         this.outputFile = outputFile;
-        
+
         try {
-			raf = new RandomAccessFile(new File(outputFile), "rw");
-			fc = raf.getChannel();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+            raf = new RandomAccessFile(new File(outputFile), "rw");
+            fc = raf.getChannel();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public void addEdges(List<Edge> es) {
@@ -51,64 +51,66 @@ public class KochManager {
             edges.add(e);
         }
     }
-    
-    public void calculateLevel(int level) {        
+
+    public void calculateLevel(int level) {
         List<Future<List<Edge>>> tasks = new ArrayList<Future<List<Edge>>>();
         // Get amount edges per side
-        int amountEdges = (int) (3 * Math.pow(4, level - 1))/3;
+        int amountEdges = (int) (3 * Math.pow(4, level - 1)) / 3;
         // Create temporary edge
         Edge tmp = new Edge(0, 0, 0, 0, Color.WHITE);
         int tmpLength = 0;
-        
+
         // Get length edge as byte array
-        try(ByteArrayOutputStream bos = new ByteArrayOutputStream()){
-            try(ObjectOutputStream oos = new ObjectOutputStream(bos)){
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+            try (ObjectOutputStream oos = new ObjectOutputStream(bos)) {
                 oos.writeObject(tmp);
             } catch (IOException ex) {
-			}
+            }
             byte[] array = bos.toByteArray();
             tmpLength = array.length;
         } catch (IOException ex) {
-		}
-        
+        }
+
         ts.init();
-    	ts.setBegin("Start Calcultating & Writing for level: " + level + "...");
-        
+        ts.setBegin("Start Calcultating & Writing for level: " + level + "...");
+
         for (int i = 0; i < 3; i++) {
-        	// Create mapped byte buffer split in 3 sections
-        	// Each section for each kochtask
-        	// app me for explanation 06-21199476
-        	MappedByteBuffer out = null;
-			try {
-				out = fc.map(MapMode.READ_WRITE, i*tmpLength*amountEdges, tmpLength*amountEdges);
-			} catch (IOException e) {
-			}
-            //KochTask run = new KochTask(this, i, level, out);
-			KochTask run = new KochTask(this, i, level, fc);
+            // Create mapped byte buffer split in 3 sections
+            // Each section for each kochtask
+            // app me for explanation 06-21199476
+            MappedByteBuffer out = null;
+            try {
+                out = fc.map(MapMode.READ_WRITE, i * tmpLength * amountEdges, tmpLength * amountEdges);
+            } catch (IOException e) {
+            }
+            KochTask run = new KochTask(this, i, level, out);
+            //KochTask run = new KochTask(this, i, level, fc);
             tasks.add(pool.submit(run));
         }
-        
+
         pool.execute(new Runnable() {
-        	public void run() {
-            	for(Future<List<Edge>> fut : tasks) {
-            		try {
-						addEdges(fut.get());
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					} catch (ExecutionException e) {
-						e.printStackTrace();
-					}
-            	}
-            	try {
-            		ts.setEnd("Ended Calculating & Writing for level: " + level + "...");
-            		System.out.println(ts.toString());
-            		fc.close();
-            		raf.close();
-            		System.exit(0);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-        	}
+            public void run() {
+                for (Future<List<Edge>> fut : tasks) {
+                    try {
+                        addEdges(fut.get());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    }
+                }
+                try {
+                    ts.setEnd("Ended Calculating & Writing for level: " + level + "...");
+                    System.out.println(ts.toString());
+                    File file = new File(outputFile);
+                    file.renameTo(new File("/home/arnoudbevers/Desktop/edges" + level + "DONE.bin"));
+                    fc.close();
+                    raf.close();
+                    System.exit(0);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         });
     }
 }
--- JSF32KochFractalFX Console - Week 13/src/jsf32kochfractalfx/JSF32KochFractal.java
+++ JSF32KochFractalFX Console - Week 13/src/jsf32kochfractalfx/JSF32KochFractal.java
@@ -5,20 +5,20 @@ import timeutil.TimeStamp;
 
 public class JSF32KochFractal {
 
-	public static void main(String[] args) {
-		int level = 10;
-		
-		String outputFile = "C:\\Users\\bramh\\Documents\\test" + level + ".bin";
-		KochManager manager = new KochManager(outputFile);
-		manager.calculateLevel(level);
-	}
-	
-	public static boolean isInteger(String str) {
-		try {
-			Integer.parseInt(str);
-		} catch(NumberFormatException e) {
-			return false;
-		}
-		return true;
-	}
+    public static void main(String[] args) {
+        int level = 4;
+
+        String outputFile = "/home/arnoudbevers/Desktop/edges4.bin";
+        KochManager manager = new KochManager(outputFile);
+        manager.calculateLevel(level);
+    }
+
+    public static boolean isInteger(String str) {
+        try {
+            Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
 }
